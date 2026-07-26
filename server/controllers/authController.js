4b1933d3e1a9b69916bb3a7513f3cd365db80cf1
rajatpanchal980 <panchalrajat774@gmail.com>
2026-07-26 08:54:57 +0530

    fix the cross site cokkie

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -3,8 +3,8 @@ const jwt = require('jsonwebtoken');
 
 const cookieOptions = {
   httpOnly: true,
-  sameSite: 'lax',
-  secure: process.env.NODE_ENV === 'production',
+  sameSite: 'none',
+  secure: true,
   maxAge: 8 * 60 * 60 * 1000,
 };
 
